7eea6c19cb5d393914d9f9c03dd38d1a3d8e7dd1
Ted <ted.ekeroth@gmail.com>
2022-04-27 20:11:00 +0200

    Add files via upload

---
+++ ItineroMainForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using BenchmarkDotNet.Running;
+using Itinero;
+using Itinero.LocalGeo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ItineroMainForm
+{
+    public partial class Form1 : Form
+    {
+        public IEnumerable<Coordinate> PossibleCoordinates => new[] { new Coordinate(55.689458f, 13.212871f), new Coordinate(55.712343f, 13.182219f), new Coordinate(55.717278f, 13.197442f), new Coordinate(55.666078f, 13.347477f), new Coordinate(55.634178f, 13.497319f) };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                RouterCache.Init();
+                Debug.WriteLine("Init done");
+                Invoke(new Action(() => { button2.Enabled = true; }));
+            });
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Init
+            RouterCache.GetRouter().Calculate(Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), PossibleCoordinates.First(), PossibleCoordinates.Last());
+            Random random = new Random(DateTime.Now.Millisecond);
+            Stopwatch sw = new Stopwatch();
+
+            int count = 1000;
+            long totalElapsed = 0;
+            long highest = 0;
+            long lowest = int.MaxValue;
+            for (int i = 0; i < count; i++)
+            {
+                var from = PossibleCoordinates.ElementAt(random.Next(0, PossibleCoordinates.Count()));
+                var to = PossibleCoordinates.ElementAt(random.Next(0, PossibleCoordinates.Count()));
+
+                sw.Start();
+                var route = RouterCache.GetRouter().Calculate(Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), from, to);
+                sw.Stop();
+                totalElapsed += sw.ElapsedMilliseconds;
+                if (sw.ElapsedMilliseconds > highest)
+                    highest = sw.ElapsedMilliseconds;
+                if (sw.ElapsedMilliseconds < lowest)
+                    lowest = sw.ElapsedMilliseconds;
+                sw.Restart();
+            }
+            double avg = totalElapsed / count;
+            List<string> asd = new List<string>();
+            asd.Add("RESULTS");
+            asd.Add("RESULTS");
+            asd.Add($"Avg:\t{avg}");
+            asd.Add($"Highest:\t{highest}");
+            asd.Add($"Lowest:\t{lowest}");
+
+            textBox1.Text = string.Join(Environment.NewLine, asd);
+        }
+    }
+}
---
+++ ItineroMainForm/ItineroHandler.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using Itinero;
+using Itinero.IO.Osm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItineroMainForm
+{
+    internal class ItineroHandler
+    {
+        [Benchmark]
+        public Route GetRoute(float fromLat, float fromLon, float toLat, float toLon)
+        {
+            // get a profile.
+            var profile = Itinero.Osm.Vehicles.Vehicle.Car.Fastest(); // the default OSM car profile.
+
+            // create a routerpoint from a location.
+            // snaps the given location to the nearest routable edge.
+            var start = RouterCache.GetRouter().Resolve(profile, fromLat, fromLon);
+            var end = RouterCache.GetRouter().Resolve(profile, toLat, toLon);
+
+            // calculate a route.
+            return RouterCache.GetRouter().Calculate(profile, start, end);
+        }
+    }
+}
