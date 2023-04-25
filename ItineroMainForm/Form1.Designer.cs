1d7a26c71346a85853d0d95c196778a7b48fbfd9
Ted <ted.ekeroth@gmail.com>
2023-04-25 10:12:44 +0200

    Add files via upload

--- ItineroMainForm/Form1.Designer.cs
+++ ItineroMainForm/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -46,9 +50,9 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(12, 53);
+            this.button2.Location = new System.Drawing.Point(931, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.Size = new System.Drawing.Size(176, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Run tests";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,17 +60,58 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Location = new System.Drawing.Point(931, 54);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(649, 575);
+            this.textBox1.Size = new System.Drawing.Size(380, 854);
             this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(334, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Choose file and import...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected file:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(334, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Import from file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 149);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(334, 759);
+            this.textBox2.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1323, 920);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,6 +127,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- ItineroMainForm/Form1.cs
+++ ItineroMainForm/Form1.cs
@@ -7,39 +7,109 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ItineroMainForm
 {
     public partial class Form1 : Form
     {
-        public IEnumerable<Coordinate> PossibleCoordinates => new[] { new Coordinate(55.689458f, 13.212871f), new Coordinate(55.712343f, 13.182219f), new Coordinate(55.717278f, 13.197442f), new Coordinate(55.666078f, 13.347477f), new Coordinate(55.634178f, 13.497319f) };
-
+        private string LAST_FILENAME_FILE = "lastfile.txt";
+        private string lastFilename = null;
+        private string fileContent = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void LoadFile()
+        {
+            if (!File.Exists(LAST_FILENAME_FILE))
+                return;
+
+            lastFilename = File.ReadAllText(LAST_FILENAME_FILE);
+            label1.Text = new FileInfo(lastFilename).FullName;
+            fileContent = File.ReadAllText(lastFilename);
+
+            textBox2.Text = "";
+            var lines = File.ReadAllLines(lastFilename);
+            var text = "";
+            foreach (var line in lines)
+            {
+                text += line + Environment.NewLine;
+            }
+            textBox2.Text = text;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             ThreadPool.QueueUserWorkItem(o =>
             {
-                textBox1.Text = "Starting RouterCache init" + Environment.NewLine;
+                Invoke(new Action(() =>
+                {
+                    textBox1.Text = "Starting RouterCache init" + Environment.NewLine;
+                }));
                 RouterCache.Init();
-                textBox1.Text += "RouterCache init done";
-                Invoke(new Action(() => { button2.Enabled = true; }));
+
+                Invoke(new Action(() =>
+                {
+                    textBox1.Text += "RouterCache init done";
+                    button2.Enabled = true;
+                }));
             });
             
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            var profile = Itinero.Osm.Vehicles.Vehicle.Car.Fastest();
             // Init
-            RouterCache.GetRouter().Calculate(Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), PossibleCoordinates.First(), PossibleCoordinates.Last());
+            List<RouterPoint> PossibleCoordinates = new List<RouterPoint>();
+            CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.CurrencyDecimalSeparator = ".";
+            int failCount = 0;
+            foreach (string line in textBox2.Lines)
+            {
+                try
+                {
+                    var pairs = line.Split(',');
+                    var p1s = pairs.First();
+                    var p2s = pairs.Last();
+
+                    float p1 = float.Parse(p1s, CultureInfo.InvariantCulture);
+                    float p2 = float.Parse(p2s, CultureInfo.InvariantCulture);
+                    try
+                    {
+                        var a = RouterCache.GetRouter().Resolve(profile, p1, p2, 200);
+                        PossibleCoordinates.Add(a);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine($" https://maps.google.com/?q={line}");
+                        failCount++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    failCount++;
+                }
+            };
+
+            RouterCache.GetRouter().Calculate(profile, PossibleCoordinates.First(), PossibleCoordinates.Last()); //init
+            
             Random random = new Random(DateTime.Now.Millisecond);
             Stopwatch sw = new Stopwatch();
 
@@ -47,21 +117,37 @@ namespace ItineroMainForm
             long totalElapsed = 0;
             long highest = 0;
             long lowest = int.MaxValue;
+             var weight = RouterCache.GetRouter().GetDefaultWeightHandler(profile);
+
+            Stopwatch total = Stopwatch.StartNew();
             for (int i = 0; i < count; i++)
             {
-                var from = PossibleCoordinates.ElementAt(random.Next(0, PossibleCoordinates.Count()));
-                var to = PossibleCoordinates.ElementAt(random.Next(0, PossibleCoordinates.Count()));
+                var c1 = random.Next(0, PossibleCoordinates.Count());
+                var c2 = random.Next(0, PossibleCoordinates.Count());
+                var from = PossibleCoordinates.ElementAt(c1);
+                var to = PossibleCoordinates.ElementAt(c2);
 
-                sw.Start();
-                var route = RouterCache.GetRouter().Calculate(Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), from, to);
-                sw.Stop();
-                totalElapsed += sw.ElapsedMilliseconds;
-                if (sw.ElapsedMilliseconds > highest)
-                    highest = sw.ElapsedMilliseconds;
-                if (sw.ElapsedMilliseconds < lowest)
-                    lowest = sw.ElapsedMilliseconds;
-                sw.Reset();
+                try
+                {
+                    sw.Start();
+                    // var something = RouterCache.GetRouter().TryCalculateRaw(profile, weight, from, to);
+
+                    var route = RouterCache.GetRouter().Calculate(profile, from, to);
+                    sw.Stop();
+                    totalElapsed += sw.ElapsedMilliseconds;
+                    if (sw.ElapsedMilliseconds > highest)
+                        highest = sw.ElapsedMilliseconds;
+                    if (sw.ElapsedMilliseconds < lowest)
+                        lowest = sw.ElapsedMilliseconds;
+                    sw.Reset();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + " | " + from  + " | " + to + " | " + $" https://www.google.com/maps/dir/{from.Latitude.ToInvariantString()},{from.Longitude.ToInvariantString()}/{to.Latitude.ToInvariantString()},{to.Longitude.ToInvariantString()}");
+                    sw.Reset();
+                }
             }
+            total.Stop();
             double avg = totalElapsed / count;
             List<string> asd = new List<string>();
 
@@ -69,8 +155,30 @@ namespace ItineroMainForm
             asd.Add($"Avg:\t{avg}");
             asd.Add($"Highest:\t{highest}");
             asd.Add($"Lowest:\t{lowest}");
+            asd.Add($"Total: {total.ElapsedMilliseconds}");
 
             textBox1.Text = string.Join(Environment.NewLine, asd);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV (*.csv)|*.csv";
+            openFileDialog.ShowDialog();
+
+            if (!File.Exists(openFileDialog.FileName))
+            {
+                MessageBox.Show("File does not exist)");
+                return;
+            }
+
+            File.WriteAllText(LAST_FILENAME_FILE, openFileDialog.FileName);
+            LoadFile();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LoadFile();
         }
     }
 }
